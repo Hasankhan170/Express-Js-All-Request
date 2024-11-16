06eb92e9883124309e79270747c4dd661c01bd34
Hasankhan170 <hasankhanali0316@gmail.com>
2024-11-16 11:05:06 +0500

    add and get req done

--- index.js
+++ index.js
@@ -3,10 +3,46 @@ import cors from "cors";
 const app = express()
 const port = 3000
 
+const TodoArr = []
+
+app.use(express.json())
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//user add 
+app.post('/todo',(req,res)=>{
+    const {title} = req.body;
+    if(!title){
+        res.status(404).json({
+            massage :"title is Required"
+        })
+        return
+    }
+
+    TodoArr.push({
+        title,
+        id : Date.now()
+    })
+
+    res.status(200).json({
+        massage :"todo is created successfully",
+        data : TodoArr
+    })
+})
+
+//user get
+app.get('/todos',(req,res)=>{
+    res.status(200).json({
+        massage:'get all todo',
+        data:TodoArr
+    })
+})
+
+//user edit
+app.put("/todos/:id",(req,res)=>{})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
